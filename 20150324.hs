6999fbc9633fd77ae932fd9cc2d47a58c38fdc2c
FilipeJrd <filipejrd@hotmail.com>
2015-03-24 12:03:48 -0300

    fim de aula

--- 20150324.hs
+++ 20150324.hs
@@ -16,3 +16,54 @@ mergesort  [x]  = [x]
 mergesort  xs = merge  (mergesort  xs1) (mergesort  xs2)
   where
     (xs1,xs2) = split xs
+
+------------------------------------------
+--aula
+
+
+menor :: Int -> Int -> Int -> Int
+menor a b c | (a <= b) && (a <= c) = a
+			| (b <= a) && (b <= c) = b
+			| otherwise = c
+
+maior :: Int -> Int -> Int -> Int
+maior a b c | (a >= b) && (a >= c) = a
+			| (b >= a) && (b >= c) = b
+			| otherwise = c
+meio :: Int -> Int -> Int -> Int
+meio a b c | ((a >= b) && (a <= c)) || ((a >= c) && (a <= b)) = a
+		   | ((b >= a) && (b <= c)) || ((b >= c) && (b <= a)) = b
+		   | otherwise = c
+
+menorMaior :: Int -> Int -> Int -> (Int,Int)
+menorMaior a b c = (menor a b c , maior a b c)
+
+ordenaTripla :: (Int, Int , Int) -> (Int , Int , Int)
+ordenaTripla (a,b,c) = (menor a b c, meio a b c, maior a b c)
+
+type Ponto = (Float, Float)
+
+type Reta = (Ponto, Ponto)
+
+
+type Pessoa = String
+
+type Livro = String
+
+type BancoDados = [(Pessoa,Livro)]
+
+firstCoord :: Ponto -> Float
+firstCoord (x,y) = x
+
+secondCoord :: Ponto -> Float
+secondCoord (x,y) = y
+
+vertical :: Reta -> Bool
+vertical ((x1,y1),(x2,y2)) | (x1 == x2) = True
+						   | otherwise = False
+
+checkReta :: Float -> Reta -> Float
+checkReta x ((x1,y1),(x2,y2))  = (((y2-y1) / (x2-x1))*(x - x1))+y1
+
+livros :: BancoDados -> Pessoa -> [Livro]
+livros b pessoa = [l | (p,l) <- b , p == pessoa]
